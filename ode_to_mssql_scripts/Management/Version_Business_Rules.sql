cf0e839f70d20fffd3dd1a7583f045cbd7aa60a8
Kate Loguteva <kate.loguteva@optimalbi.com>
2017-02-21 17:13:41 +1300

    Upgraded helper scripts to version 4

--- ode_to_mssql_scripts/Management/Version_Business_Rules.sql
+++ ode_to_mssql_scripts/Management/Version_Business_Rules.sql
@@ -1,105 +1,94 @@
-Ôªø/*
+/*
 	When business rules change, it is desirable to be able to audit the different versions of the rule over time.
-	One method of doing this is the ‚ÄúVersion‚Äù the Source Table to represent the new Rule(s).
+	One method of doing this is the ìVersionî the Source Table to represent the new Rule(s).
 */
 USE [ODE_Config];
 GO
 /******************************
 Set the Parameters here:
 *****************************/
--- Provide the name of the Source as it is in [dbo].[dv_source_table]
-DECLARE @OldSourceTableName VARCHAR(128) = 'Sales__Customer'
-	-- Provide the New name for your Source - usually suffixed with "_Vnnn" to differentiate a new version
-, @NewSourceTableName VARCHAR(128) = 'Sales__Customer_V001'
-	-- If there is a Stored Procedure, provide the new name - usually suffixed with the same "_Vnnn" as the Source.
-	-- If no stored Procedure is required, set to a Null Value ('').
-, @NewSourceProcedureSchema VARCHAR(128) = 'Stage'
+DECLARE 
+-- Provide the unique name of the Source as it is in [dbo].[dv_source_table]
+  @SourceTableName VARCHAR(128)		= 'Sales__Customer'
+	-- Provide the name of the new stored procedure - usually suffixed with the "_Vnnn"
 , @NewSourceProcedureName VARCHAR(128) = 'usp_Sales__Customer_V001'
+	-- If stored procedure can handle a load type as an input parameter and supports both Delta and Full load, set to 1. Default is 0
+, @PassLoadTypeToProc BIT			= 0
 	-- If you are building an Incremental Release, provide your Release Number
- , @ReleaseNumber INT = 0;
+, @SprintDate CHAR(8)				= '20170131'
+	-- Start Date of the current Sprint in Integer yyyymmdd (this depends on having set up a Sprint Release with the key yyymmdd00
+	-- e.g. EXECUTE [dv_release].[dv_release_master_insert] 2016080100, 'Test Sprint release', 'US001', 'Jira'
+, @ReleaseReference VARCHAR(50)		= 'HR-538'
+	-- User Story and/or Task numbers for the Satellite which you are building.
+, @ReleaseSource VARCHAR(50)		= 'Jira'
+	-- system the reference number refers to, Rally, Jira etc.
 /*****************************/
 
-DECLARE @Release_Key INT;
--- Get the reease Key for use below:
-SELECT @Release_Key = [release_key] FROM
- [dv_release].[dv_release_master]
-WHERE [release_number] = @ReleaseNumber;
 BEGIN TRANSACTION;
 BEGIN TRY
+
+ ----Release
+ DECLARE  @seqint			INT
+ ,@release_key				INT
+ ,@release_number			INT
+ ,@Description				VARCHAR(256)
+ 
+/********************************************
+Release:
+********************************************/
+--'Find the Next Release for the Sprint'
+SELECT TOP 1 @seqint = cast(right(cast([release_number] AS VARCHAR(100)), len(cast([release_number] AS VARCHAR(100))) - 8) AS INT)
+FROM [dv_release].[dv_release_master]
+WHERE left(cast([release_number] AS VARCHAR(100)), 8) = @SprintDate
+ORDER BY 1 DESC
+IF @@rowcount = 0
+SET @release_number = cast(@SprintDate + '01' AS INT)
+ELSE
+SET @release_number = cast(@SprintDate + right('00' + cast(@seqint + 1 AS VARCHAR(100)), 2) AS INT)
+SELECT @release_number
+SET @Description = 'Create new version of source business rule: ' + quotename(@SourceTableName)
+-- Create the Release:
+EXECUTE  @release_key = [dv_release].[dv_release_master_insert]  @release_number		= @release_number	-- date of the Sprint Start + ad hoc release number
+																,@release_description	= @Description		-- what the release is for
+																,@reference_number		= @ReleaseReference
+																,@reference_source		= @ReleaseSource
+-----------------------------------------------------------------
  -- Create the New Source:
- DECLARE @System_key INT
- , @Source_table_schema VARCHAR(128)
- , @Source_table_name VARCHAR(128)
- , @Source_table_load_type VARCHAR(50)
- , @Source_procedure_schema VARCHAR(128)
- , @Source_procedure_name VARCHAR(128)
- , @Release_number INT
- , @New_source_table_key INT;
+ DECLARE 
+  @Source_table_key		INT
+, @Old_source_version	INT
+, @New_source_version	INT
+
  SELECT @Release_Key
- , @ReleaseNumber;
- SELECT @System_key = [system_key]
- , @Source_table_schema = [source_table_schema]
- , @Source_table_name = @NewSourceTableName
- , @Source_table_load_type = [source_table_load_type]
- , @Source_procedure_schema = @NewSourceProcedureSchema
- , @Source_procedure_name = @NewSourceProcedureName
- , @Release_number = @ReleaseNumber
+ , @Release_Number;
+
+ -- Get the source table key
+ SELECT @Source_table_key = [source_table_key]
  FROM [dbo].[dv_source_table]
- WHERE [source_table_name] = @OldSourceTableName;
- SELECT @System_key
- , @Source_table_schema
- , @Release_number;
- EXECUTE @New_source_table_key = [dbo].[dv_source_table_insert]
- @System_key = @System_key
- , @Source_table_schema = @Source_table_schema
- , @Source_table_name = @Source_table_name
- , @Source_table_load_type = @Source_table_load_type
- , @Source_procedure_schema = @Source_procedure_schema
- , @Source_procedure_name = @Source_procedure_name
- , @is_retired = 0
- , @Release_number = @Release_number;
- -- Hook All Columns, which were related to the Old Source, to the New Source:
- UPDATE [dbo].[dv_column]
- SET
- [table_key] = @New_source_table_key
- , [release_key] = @Release_Key FROM [dbo].[dv_source_system] [ss]
- INNER JOIN [dbo].[dv_source_table] [st]
- ON [ss].[source_system_key] = [st].[system_key]
- INNER JOIN [dbo].[dv_column]
- ON [dv_column].[table_key] = [st].[source_table_key]
- WHERE [st].[source_table_name] = @OldSourceTableName;
- -- Hook the new Source into the place in the Loading Hierarchy, where The Old Source used to be:
- UPDATE [sth]
- SET
- [source_table_key] = @New_source_table_key
- , [release_key] = @Release_Key FROM [dbo].[dv_source_system] [ss]
- INNER JOIN [dbo].[dv_source_table] [st]
- ON [ss].[source_system_key] = [st].[system_key]
- INNER JOIN [dv_scheduler].[dv_source_table_hierarchy] [sth]
- ON [sth].[source_table_key] = [st].[source_table_key]
- WHERE [st].[source_table_name] = @OldSourceTableName;
- -- Hook the New source into the Schedule(s) to which the Old source belonged:
- UPDATE [sth]
- SET
- [prior_table_key] = @New_source_table_key
- , [release_key] = @Release_Key FROM [dbo].[dv_source_system] [ss]
- INNER JOIN [dbo].[dv_source_table] [st]
- ON [ss].[source_system_key] = [st].[system_key]
- INNER JOIN [dv_scheduler].[dv_source_table_hierarchy] [sth]
- ON [sth].[prior_table_key] = [st].[source_table_key]
- WHERE [st].[source_table_name] = @OldSourceTableName;
- UPDATE [sst]
- SET
- [source_table_key] = @New_source_table_key
- , [release_key] = @Release_Key FROM [dbo].[dv_source_system] [ss]
- INNER JOIN [dbo].[dv_source_table] [st]
- ON [ss].[source_system_key] = [st].[system_key]
- INNER JOIN [dv_scheduler].[dv_schedule_source_table] [sst]
- ON [sst].[source_table_key] = [st].[source_table_key]
- WHERE [st].[source_table_name] = @OldSourceTableName;
-UPDATE [dbo].[dv_source_table]
-SET [is_retired] = 1 
- WHERE [source_table_name] = @OldSourceTableName;
+ WHERE [source_unique_name] = @SourceTableName;
+
+ -- Get the current source version
+ SELECT @Old_source_version = MAX([source_version])
+ FROM [dbo].[dv_source_version]
+ WHERE [source_table_key] = @Source_table_key
+
+ -- Create a new version
+ SET @New_source_version = @Old_source_version + 1
+
+ -- Retire all previous versions of the source table
+ UPDATE [dbo].[dv_source_version]
+ SET [is_current] = 0
+ WHERE [source_table_key] = @Source_table_key
+
+ -- Insert the new version
+ EXECUTE [dbo].[dv_source_version_insert]
+  @source_table_key = @Source_table_key
+, @source_version = @New_source_version
+, @source_procedure_name = @NewSourceProcedureName
+, @pass_load_type_to_proc = @PassLoadTypeToProc
+, @is_current = 1
+, @release_number = @Release_Number
+ 
  PRINT 'succeeded';
  -- Commit if successful:
  COMMIT;
